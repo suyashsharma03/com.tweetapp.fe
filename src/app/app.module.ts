3837f3dda8d857b6615945036cd1e4800fbb258a
SUYASH SHARMA <suyash03041996@gmail.com>
2022-07-31 02:21:15 +0530

    added login register and tweet components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { LoginComponent } from "./main/login/component/login.component";
+import { RegisterComponent } from "./main/register/component/register.component";
+import { TweetComponent } from "./main/tweet/component/tweet.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TweetComponent
   ],
   imports: [
     BrowserModule,
